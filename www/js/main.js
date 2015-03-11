1e4defac749bbddb8526ea5b325ce7d35e59146e
bwswedberg <bwswedberg@gmail.com>
2015-03-11 10:14:54 -0400

    remove unneeded dependencies from require config

--- www/js/main.js
+++ www/js/main.js
@@ -10,24 +10,20 @@ require.config({
         'd3': 'lib/d3',
         'crossfilter': 'lib/crossfilter',
         '$.calendars': 'lib/jquery.calendars',
-        'metricsgraphics': 'lib/metricsgraphics',
         'ol': 'lib/ol',
         'papaparse': 'lib/papaparse',
         'colorbrewer': 'lib/colorbrewer',
-        'bootstrap': 'lib/bootstrap',
-        'randomcolor': 'lib/randomcolor'
+        'bootstrap': 'lib/bootstrap'
     },
     shim: {
         'jquery': {exports: '$'},
         'd3': {exports: 'd3'},
         'crossfilter': {exports: 'crossfilter'},
         '$.calendars': {exports: '$.calendars', deps: ['jquery']},
-        'metricsgraphics': {exports: 'MG', deps: ['jquery', 'd3']},
         'ol': {exports: 'ol'},
         'papaparse': {exports: 'Papa'},
         'colorbrewer': {exports: 'colorbrewer'},
-        'bootstrap': {deps: ['jquery']},
-        'randomcolor': {exports: 'randomcolor'}
+        'bootstrap': {deps: ['jquery']}
     }
 });
 
